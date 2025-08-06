2f33c9723b16613c6a782f771cbeb193a75b988e
Chirag-varu <chiragvaru03@gmail.com>
2025-08-06 21:23:34 +0530

    deployment big fix jwt token missing

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ dotenv.config({});
 connectDB();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const __dirname = path.resolve();
 
 // default middleware
