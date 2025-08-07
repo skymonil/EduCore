fe0970dde0b1793a3fa203ace67ef59c11a9beff
skymonil <monilparikh11@gmail.com>
2025-08-07 19:53:46 +0530

    Implemented DB connection pooling to decrease latency, handle multiple users simultaneously

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, { 
+             maxPoolSize: 10, // max 10 connections in the pool
+         });;
         console.log('MongoDB Connected');
     } catch (error) {
         console.log("error occured", error); 
